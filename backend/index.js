283afb164f52115a214fc643c2feacb8d81a63bd
Mohd Haider <86488954+mohdhaider07@users.noreply.github.com>
2022-11-18 19:47:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv"); 
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
